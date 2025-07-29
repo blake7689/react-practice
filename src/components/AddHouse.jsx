437de2bab5f633f48e5a2107b1523669071ce582
blake7689 <blakec7689@gmail.com>
2025-07-29 17:45:41 +0100

    Added add house image tyest functionality

--- src/components/AddHouse.jsx
+++ src/components/AddHouse.jsx
@@ -1,4 +1,5 @@
 import { useState, useTransition } from "react";
+import styles from "./AddHouse.module.css";
 
 const AddHouse = ( { addHouse } ) => {
     const [isPending, startTransition] = useTransition();
@@ -7,69 +8,104 @@ const AddHouse = ( { addHouse } ) => {
         address: "",
         country: "",
         price: "",
+        photo: null,
+        photoFile: null,
     };
 
     const [newHouse, setNewHouse] = useState(emptyHouse);
 
     const onHouseSubmitClick = () => { 
         startTransition(async() => { 
-            await addHouse(newHouse);   
+            await addHouse({
+                ...newHouse,
+                photo: newHouse.photoFile
+                    ? newHouse.photoFile.name.replace(/\.[^/.]+$/, "")
+                    : null,
+                photoFile: undefined 
+            });   
         });
         setNewHouse(emptyHouse);
     };
 
     return (
-        <div className="row align-items-center mb-3">
-            <div className="col">
-                <input
-                    id="Address"
-                    className="form-control"
-                    type="text"
-                    value={newHouse.address}
-                    onChange={(e) =>
-                        setNewHouse({
-                            ...newHouse,
-                            address: e.target.value
-                        })
-                    }
-                    placeholder="Address"
-                />
+        <div className="mb-3">
+            <div className="row align-items-center mb-2">
+                <div className="col">
+                    <input
+                        id="Address"
+                        className="form-control"
+                        type="text"
+                        value={newHouse.address}
+                        onChange={(e) =>
+                            setNewHouse({
+                                ...newHouse,
+                                address: e.target.value
+                            })
+                        }
+                        placeholder="Address"
+                    />
+                </div>
+                <div className="col">
+                    <input
+                        id="country"
+                        className="form-control"
+                        type="text"
+                        value={newHouse.country}
+                        onChange={(e) =>
+                            setNewHouse({
+                                ...newHouse,
+                                country: e.target.value
+                            })
+                        }
+                        placeholder="Country"
+                    />
+                </div>
+                <div className="col">
+                    <input
+                        id="price"
+                        className="form-control"
+                        type="number"
+                        value={newHouse.price}
+                        onChange={(e) =>
+                            setNewHouse({
+                                ...newHouse,
+                                price: e.target.value === '' ? '' : parseInt(e.target.value)
+                            })
+                        }
+                        placeholder="Asking Price"
+                    />
+                </div>
             </div>
-            <div className="col">
-                <input
-                    id="country"
-                    className="form-control"
-                    type="text"
-                    value={newHouse.country}
-                    onChange={(e) =>
-                        setNewHouse({
-                            ...newHouse,
-                            country: e.target.value
-                        })
-                    }
-                    placeholder="Country"
-                />
-            </div>
-            <div className="col">
-                <input
-                    id="price"
-                    className="form-control"
-                    type="number"
-                    value={newHouse.price}
-                    onChange={(e) =>
-                        setNewHouse({
-                            ...newHouse,
-                            price: e.target.value === '' ? '' : parseInt(e.target.value)
-                        })
-                    }
-                    placeholder="Asking Price"
-                />
+            <div className="row align-items-center">
+                <div className={`col-auto ${styles.fileInputBox}`}>
+                    <input
+                        id="photo"
+                        className="form-control"
+                        type="file"
+                        accept="image/*"
+                        onChange={(e) =>
+                            setNewHouse({
+                                ...newHouse,
+                                photoFile: e.target.files[0] || null
+                            })
+                        }
+                    />
+                </div>
+                <div className="col-auto">
+                    <button className={`btn btn-primary ${styles.addHouseButton}`} 
+                        onClick={onHouseSubmitClick} disabled={isPending}>
+                        Add
+                    </button>
+                </div>
             </div>
             <div className="col-auto">
-                <button className="btn btn-primary"
-                    onClick={onHouseSubmitClick} disabled={isPending}>
-                    Add
-                </button>
+                {newHouse.photoFile && (
+                    <img
+                        src={URL.createObjectURL(newHouse.photoFile)}
+                        alt="Preview"
+                        className={styles.imagePreview}
+                    />
+                )}
             </div>
         </div>
     );
